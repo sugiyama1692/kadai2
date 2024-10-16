ebc3294a83d8af0223c6c08c71723a6110915ac9
sugiyama1692 <kyoei.engine.mba2408.starlight@gmail.com>
2024-10-16 23:53:02 +0900

    遷移機能、入力値の受け渡し機能

---
+++ SecondKadaiApp/ResultViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ResultViewController.swift
+//  SecondKadaiApp
+//
+//  Created by mba2408.starlight kyoei.engine on 2024/10/16.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    
+    @IBOutlet weak var label: UILabel!
+    
+    var str = ""
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        label.text = str
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- SecondKadaiApp/ViewController.swift
+++ SecondKadaiApp/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toNext" {
+            let nextView = segue.destination as! ResultViewController
+            nextView.str = textField.text!
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func unwind(_ segue: UIStoryboardSegue) {
+    }
 }
+
 
