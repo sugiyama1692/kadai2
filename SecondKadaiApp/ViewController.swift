74a5d8895f21d301db7451ff4f8144950fffa6da
sugiyama1692 <kyoei.engine.mba2408.starlight@gmail.com>
2024-10-16 21:38:32 +0900

    Initial Commit

---
+++ SecondKadaiApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SecondKadaiApp
+//
+//  Created by mba2408.starlight kyoei.engine on 2024/10/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
